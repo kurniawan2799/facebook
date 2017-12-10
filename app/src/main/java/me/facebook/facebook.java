bce7651acbe57a38ca6c2d5749683fa214c973c2
kurniawan2799 <izza.nada@gmail.com>
2017-12-10 23:01:56 +0700

    Initial commit

---
+++ app/src/main/java/me/facebook/facebook.java
@@ -0,0 +1,13 @@
+package me.facebook;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class facebook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook);
+    }
+}
